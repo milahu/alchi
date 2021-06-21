4843b991d5ff286c689bd0e75486e4d1126b08fd
milahu <milahu@gmail.com>
2021-06-21 13:21:25 +0200

    alchi-faces: add bigfive2face.html

---
+++ src/alchi-faces/images/iq.hse.ru/get-images.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl -o opennes_b.png https://iq.hse.ru/pubs/share/direct/370140819.png
+curl -o conscient_a.png https://iq.hse.ru/pubs/share/direct/370140809.png
+curl -o extraversion1.png https://iq.hse.ru/pubs/share/direct/370140555.png
+curl -o agreeable2.png https://iq.hse.ru/pubs/share/direct/370140598.png
+curl -o neuro.png https://iq.hse.ru/pubs/share/direct/370140604.png
+
+# images were manually cropped to a-high.png, a-low.png, ...
