ee668a0e239ca4528b54914c8bb02842bc3320b7
Milan Hauth <milahu@gmail.com>
2022-07-08 10:37:25 +0200

    add src/scripts/sync-git-mirrors.sh

---
+++ src/scripts/sync-git-mirrors.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# main repo
+# https://github.com/milahu/alchi
+git pull github master
+
+# https://gitlab.com/milahu/alchi
+git push gitlab
+
+# https://try.gitea.io/milahu/alchi
+# this is an automatic mirror
+# updates are pulled by try.gitea.io
+#git push gitea
+
+# http://it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/milahu/alchi
+torsocks git push darktea
