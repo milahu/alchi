89346d36fc7f31b7240f8081079d431017d4bb09
Milan Hauth <milahu@gmail.com>
2022-07-19 12:39:42 +0200

    also sync git tags

--- src/scripts/sync-git-mirrors.sh
+++ src/scripts/sync-git-mirrors.sh
@@ -5,18 +5,18 @@ set -x
 
 # main repo
 # https://github.com/milahu/alchi
-#git push github # TODO
-git pull github master
+git pull github master --tags
+git push github --tags
 
 # https://gitlab.com/milahu/alchi
-git push gitlab
+git push gitlab --tags
 
 # https://try.gitea.io/milahu/alchi
 # this is an automatic mirror
 # updates are pulled by try.gitea.io
 #git push gitea
 
-git push srht
+git push srht --tags
 
 # http://it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/milahu/alchi
-torsocks git push darktea
+torsocks git push darktea --tags
