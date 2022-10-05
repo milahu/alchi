1b008da843578831d1bdb51ca37c4ff4b29126a0
Milan Hauth <milahu@gmail.com>
2022-10-05 11:53:39 +0200

    pass args to git push

--- src/scripts/sync-git-mirrors.sh
+++ src/scripts/sync-git-mirrors.sh
@@ -3,25 +3,28 @@
 set -e
 set -x
 
+args=("$@")
+
 # main repo
 # https://github.com/milahu/alchi
-git pull github master
-git pull github master --tags
-git push github
-git push github --tags
+#git pull github master
+#git pull github master --tags
+
+git push github "${args[@]}"
+git push github --tags "${args[@]}"
 
 # https://gitlab.com/milahu/alchi
-git push gitlab
-git push gitlab --tags
+git push gitlab "${args[@]}"
+git push gitlab --tags "${args[@]}"
 
 # https://try.gitea.io/milahu/alchi
 # this is an automatic mirror
 # updates are pulled by try.gitea.io
 #git push gitea
 
-git push srht
-git push srht --tags
+git push srht "${args[@]}"
+git push srht --tags "${args[@]}"
 
 # http://it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/milahu/alchi
-torsocks git push darktea
-torsocks git push darktea --tags
+torsocks git push darktea "${args[@]}"
+torsocks git push darktea --tags "${args[@]}"
