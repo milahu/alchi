6951b576e258cf5f5da5c993410af32ba722e77a
Milan Hauth <milahu@gmail.com>
2022-11-15 11:16:11 +0100

    add bootstrap

---
+++ src/whoaremyfriends/src/bootstrap/update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# -N = timestamping
+
+wget -N \
+  https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.css \
+  https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.min.css \
+  https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.min.css.map \
+  https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/js/bootstrap.js \
+  https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/js/bootstrap.min.js \
+  https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/js/bootstrap.min.js.map \
+  https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/js/bootstrap.bundle.js \
+  https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/js/bootstrap.bundle.min.js \
+  https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/js/bootstrap.bundle.min.js.map \
