1dd7eb2c001ac67e0934225de88324270a88a8d4
Milan Hauth <milahu@gmail.com>
2022-12-14 14:46:36 +0100

    force push

--- src/scripts/sync-git-mirrors.sh
+++ src/scripts/sync-git-mirrors.sh
@@ -3,36 +3,38 @@
 set -e
 set -x
 
+opts="--force"
+
 git stash -m "git pull-push $(date)"
 
 # main repo
 # https://github.com/milahu/alchi
 git pull github master
 git pull github master --tags
-git push github
-git push github --tags
+git push github $opts
+git push github --tags $opts
 
 # https://gitlab.com/milahu/alchi
-git push gitlab
-git push gitlab --tags
+git push gitlab $opts
+git push gitlab --tags $opts
 
 # https://try.gitea.io/milahu/alchi
 # this is an automatic mirror
 # updates are pulled by try.gitea.io
 #git push gitea
 
-git push srht
-git push srht --tags
+git push srht $opts
+git push srht --tags $opts
 
 # http://it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/milahu/alchi
 (
-torsocks git push darktea &&
-torsocks git push darktea --tags
+torsocks git push darktea $opts &&
+torsocks git push darktea --tags $opts
 ) ||
 (
 echo retrying darktea
-torsocks git push darktea &&
-torsocks git push darktea --tags
+torsocks git push darktea $opts &&
+torsocks git push darktea --tags $opts
 )
 
 git stash pop
