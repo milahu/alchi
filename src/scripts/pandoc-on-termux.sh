62c900e6310da9539ad6d3b2425ad3bd2a94abd6
Milan Hauth <milahu@gmail.com>
2025-02-06 14:43:37 +0100

    add pandoc-on-termux.sh

---
+++ src/scripts/pandoc-on-termux.sh
@@ -0,0 +1,58 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+# based on debian_on_termux_10.sh
+
+# TODO apt install pandoc
+
+# FIXME bind input and output files
+# pandoc -o out.html in.md
+
+# oldstable, stable, testing, unstable
+BRANCH=testing
+# base(258M), minbase(217M), buildd, fakechroot
+VAR=minbase
+# list_close_debian_mirrors.sh
+REPO=http://ftp.debian.org/debian/
+
+set -e
+trap '[ $? -eq 0 ] && exit 0 || (echo; echo "termux-info:"; termux-info)' EXIT
+
+if [ ! -d ~/debian-$BRANCH ] ; then
+	ARCH=$(uname -m)
+	case $ARCH in
+		aarch64) ARCH=arm64 ;;
+		x86_64) ARCH=amd64 ;;
+		i686) ARCH=i386 ;;
+		armv7l) ARCH=armhf ;;
+		armv8l) apt-get -qq install getconf; if [ $(getconf LONG_BIT) -eq 64 ]; then ARCH=arm64; else ARCH=armhf; fi ;;
+		*) echo "Unsupported architecture $ARCH"; exit ;;
+	esac
+	apt-get -qq update
+	apt-get -qq dist-upgrade
+	apt-get -qq install debootstrap proot wget
+	debootstrap \
+		--variant=$VAR \
+		--exclude=systemd \
+		--arch=$ARCH \
+		$BRANCH \
+		~/debian-$BRANCH \
+		$REPO
+fi
+unset LD_PRELOAD
+proot \
+	-0 \
+	--link2symlink \
+	-r ~/debian-$BRANCH \
+	-w /root \
+	-b /dev/ \
+	-b /sys/ \
+	-b /proc/ \
+	-b /data/data/com.termux/files/home \
+	-b /storage \
+        -b "$PWD" \
+	/usr/bin/env -i \
+	HOME="$PWD" \
+        PWD="$PWD" \
+	TERM="xterm-256color" \
+	PATH=/bin:/usr/bin:/sbin:/usr/sbin \
+	/usr/bin/pandoc "$@"
